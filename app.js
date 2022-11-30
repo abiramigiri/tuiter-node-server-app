03db88afbe260bc966e2299d5f90b21a9b161d9b
Abirami Giri <abiramigiri@AbiramisMacBook.cable.rcn.com>
2022-11-30 13:57:07 -0500

    assignmemt 9

--- app.js
+++ app.js
@@ -3,7 +3,25 @@ import cors from 'cors';
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
 
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/'
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false,
+    maxPoolSize: 10,
+    socketTimeoutMS: 45000,
+    family: 4
+
+}
+mongoose.connect(CONNECTION_STRING, options).then(() => {
+    console.log('connected to database')
+})
+    .catch((err) => {
+        console.log(err)
+    })
 const app = express();
 app.use(cors());
 app.use(express.json());
